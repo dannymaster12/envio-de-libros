4611c975b3c0d754d350cca40d6ca3f8b13d76e6
unknown <dannymaster12.acostaaponzadaniel@gmail.com>
2019-04-08 23:27:12 -0500

    first commit

---
+++ conexion.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+$host = 'fdb25.awardspace.net';
+$usr = '3010741_enviodelibros';
+$pass = 'danielito123';
+$db = '3010741_enviodelibros';
+
+$conn = mysqli_connect($host, $usr, $pass, $db);
+
+?>
